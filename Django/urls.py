135ecf30f611c30ffa0beb30db6c1f52e3c78a98
Евгений <Tushkov@neftisa.ru>
2024-06-06 13:02:38 +0300

    Шаблоны

--- Django/urls.py
+++ Django/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('work', views.work),
-    path('countries_list', views.countries_list),
-    path('country/<country>', views.country),
-    path('countries_list_db', views.countries_list_db),
-    path('country_db/<country>', views.country_db),
-    path('items', views.items),
-    path('item/<int:id>', views.item_id),
+    path('', views.home, name="home"),
+    path('work', views.work, name="work"),
+    path('countries_list', views.countries_list, name="countries-list"),
+    path('country/<country>', views.country, name="countryj-detail"),
+    path('countries_list_db', views.countries_list_db, name="countries-list-db"),
+    path('country_db/<country>', views.country_db, name="country-detail"),
+    path('items', views.items, name="items-list"),
+    path('item/<int:id>', views.item_id, name="item-detail"),
 
 ]
